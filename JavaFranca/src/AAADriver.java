bc3928d210272ac61b7fc43bb5009591a7ced1b4
zoox101 <william@thebookers.net>
2016-10-27 00:08:24 -0500

    Added instance class

--- JavaFranca/src/AAADriver.java
+++ JavaFranca/src/AAADriver.java
@@ -1,12 +1,30 @@
+import java.util.HashMap;
 
 public class AAADriver {
 	
+	static HashMap<String, GraphObject> abs = new HashMap<String, GraphObject>();
+	
 	public static void main(String[] args) {
-		GraphObject music = new GraphObject("Music");
-		music.addNick("Songs");
-		GraphObject artists = new GraphObject("Artists");
-		artists.add("make", music);
-		System.out.println(artists.toSentence());
+		
+		put("music");
+		put("artists");
+		put("genres");
+		put("characteristics");
+		
+		connect("artists", "make", "music");
+		connect("music", "comes in", "genres");
+		connect("genres", "have shared", "characteristics");
+		connect("music", "has", "characteristics");
+		connect("artists", "specialize in", "genres");
+		
+		print("music");
+		print("artists");
+		print("genres");
+		print("characteristics");
 	}
+	
+	static void put(String name) {abs.put(name, new GraphObject(name));}
+	static void connect(String subject, String verb, String object) {abs.get(subject).add(verb, abs.get(object));}
+	static void print(String string) {System.out.println(abs.get(string).toSentence());}
 
 }
