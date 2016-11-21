b204d4cecacc08d0abc02605af2ac4bf037af982
zoox101 <william@thebookers.net>
2016-11-21 17:17:35 -0600

    Renamed Thought Division to Parse from File

--- JavaFranca/src/ParseFromFile/Parse.java
+++ JavaFranca/src/ParseFromFile/Parse.java
@@ -1,4 +1,4 @@
-package ThoughtDivision;
+package ParseFromFile;
 import java.util.ArrayList;
 import java.util.Set;
 
--- JavaFranca/src/ParseFromFile/ZZZTesting.java
+++ JavaFranca/src/ParseFromFile/ZZZTesting.java
@@ -1,4 +1,4 @@
-package ThoughtDivision;
+package ParseFromFile;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
